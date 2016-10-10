e179a01b9d22067459bf69b85ad77cc14f0ce3e1
userv <rosen.t.angelov@gmail.com>
2016-10-10 13:29:08 +0300

    update

--- Square-Area/Program.cs
+++ Square-Area/Program.cs
@@ -10,6 +10,12 @@ namespace Square_Area
     {
         static void Main(string[] args)
         {
+            Console.Write("a=");
+            var a = int.Parse(Console.ReadLine());
+            var area = a * a;
+            Console.Write("Area=");
+            Console.WriteLine(area);
+        
         }
     }
 }
